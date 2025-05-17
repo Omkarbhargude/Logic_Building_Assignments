02c4b77ca798529abd638e5d733ee3eea5b04e3b
Omkar Bhargude <omkarbhargude29@gmail.com>
2025-05-17 22:01:28 +0530

    Add files via upload

---
+++ Assignment_9/program5.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+
+int CountFrequency(int iNo)
+{
+    int iCnt = 0;
+    int iDigit = 0;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit < 6)
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCnt;
+
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter digit :");
+    scanf("%d",&iValue);
+
+    iRet = CountFrequency(iValue);
+
+    printf("%d\n",iRet);
+
+    return 0;
+}
