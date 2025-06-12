66e9c3e7f026151c5cdf06d18e8961549d44c6fd
Omkar Bhargude <omkarbhargude29@gmail.com>
2025-06-12 19:41:51 +0530

    Add files via upload

---
+++ Assignment_015/program2.c
@@ -0,0 +1,42 @@
+//Input :- iRow = 4 , iCol = 4 
+/* O/P :-
+         * * * *
+         * * * #
+         * * # #
+         * # # #
+*/
+
+#include<stdio.h>
+
+void DisplayPattern(int iRow,int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            if(i <= j)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("#\t");
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter numbers of Rows and Columns :\n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    DisplayPattern(iValue1,iValue2);
+
+    return 0;
+}
